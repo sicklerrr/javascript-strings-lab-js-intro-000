768a5b67a9c90a17664ce808ad7389dbeef5186a
Aaron Sickler <sicklera517@gmail.com>
2019-08-14 03:41:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,4 +6,4 @@ var greetSpecialGuest = "Hello, " + specialGuest + "!";
 
 var topic = "space";
 
-var conversation = `Let\'s talk about{console.log(topic)}.`;
+var conversation = `Let\'s talk about space.`;
